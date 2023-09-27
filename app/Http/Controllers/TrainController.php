d79c2a5ae9be5a42b3f5ab518965f69eeff3eda6
dariofusco <fdario@hotmail.it>
2023-09-27 15:45:59 +0200

    stampa tabella

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 class TrainController extends Controller
 {
     public function index() {
-        return view('home');
+        $dati = Train::all();
+        return view('home', ['trains' => $dati]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +8,43 @@
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
     <title>Document</title>
 </head>
+
 <body>
-    <h1 class="text-center">Hello World <i class="fa-solid fa-hand"></i></h1>
+    <div class="container text-center">
+        <h1 class="py-3">Train <i class="fa-solid fa-train"></i></h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">azienda</th>
+                    <th scope="col">stazione_di_partenza</th>
+                    <th scope="col">stazione_di_arrivo</th>
+                    <th scope="col">orario_di_partenza</th>
+                    <th scope="col">orario_di_arrivo</th>
+                    <th scope="col">codice_treno</th>
+                    <th scope="col">numero_carrozze</th>
+                    <th scope="col">in_orario</th>
+                    <th scope="col">cancellato</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($trains as $train)
+                    <tr>
+                        <th scope="row">{{ $train->id }}</th>
+                        <td>{{ $train->azienda }}</td>
+                        <td>{{ $train->stazione_di_partenza }}</td>
+                        <td>{{ $train->stazione_di_arrivo }}</td>
+                        <td>{{ $train->orario_di_partenza }}</td>
+                        <td>{{ $train->orario_di_arrivo }}</td>
+                        <td>{{ $train->codice_treno }}</td>
+                        <td>{{ $train->numero_carrozze }}</td>
+                        <td>{{ $train->in_orario }}</td>
+                        <td>{{ $train->cancellato }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </body>
+
 </html>
