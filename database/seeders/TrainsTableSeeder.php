5512ab1226063d79ba7be5abfe75af39e4b5c07d
dariofusco <fdario@hotmail.it>
2023-09-27 15:12:39 +0200

    crea controller

---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Train;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $train = new Train();
+
+            $train->azienda = $faker->company();
+            $train->stazione_di_partenza = $faker->city();
+            $train->stazione_di_arrivo = $faker->city();
+            $train->orario_di_partenza = $faker->time();
+            $train->orario_di_arrivo = $faker->time();
+            $train->codice_treno = $faker->ean8();
+            $train->numero_carrozze = $faker->randomDigitNotNull();
+            $train->in_orario = $faker->boolean();
+            $train->cancellato = $faker->boolean();
+
+            $train->save();
+        }
+    }
+}
